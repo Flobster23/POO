a5cf71adec16cb0a594962e59356948f1d916091
Flobster23 <101504463+Flobster23@users.noreply.github.com>
2025-03-12 14:46:35 -0300

    Add files via upload

---
+++ aula 1 POO/exercicio1.java
@@ -0,0 +1,20 @@
+public class exercicio1 {
+    public static void main(String[] args) {
+        int n = 10;
+        while(n <= 25){
+            System.out.println(n);
+            n++;
+        }
+
+        n = 10;
+
+        do{
+            System.out.println(n);
+            n++;
+        }while(n <= 25);
+
+        for(n = 10; n <= 25; n++){
+            System.out.println(n);
+        }
+    }
+}
