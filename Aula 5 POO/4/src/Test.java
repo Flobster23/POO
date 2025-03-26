063256a1a3453efdc5c96370a13997ff21e25c1b
Flobster23 <101504463+Flobster23@users.noreply.github.com>
2025-03-26 14:20:48 -0300

    Add files via upload

---
+++ Aula 5 POO/4/src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    private int a;
+
+    public Test (int a) {
+        this.a = a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+    public int getA() {
+        return a;
+    }
+}
